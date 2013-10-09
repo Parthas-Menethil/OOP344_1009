d5b54be3727ca9d14fb6110778796c27cbaf8be0
Parthas-Menethil <xywhpanda@gmail.com>
2013-10-09 12:09:57 -0400

    Implement the basic event system

--- Source.cpp
+++ Source.cpp
@@ -5,7 +5,8 @@ private:
 protected:
 public:
 	void (*over100)(int&); // Pointer
-	void (*under0)(int&);  // Pooter
+	void (*under0)(int&);  // Pointer
+	void setVal(int i) { val = i; }
 	EventSystem() : val(0), over100(0), under0(0){
 	
 	}
@@ -18,8 +19,22 @@ public:
 		}
 	}
 };
-void safeZero(){}
+void safeZero(int& i){
+	i = 0;
+}
+void inform100(int& i){
+	std::cout << "The var became " << i << std::endl;
+}
 int main(){
-
+	bool done = false;
+	EventSystem es;
+	es.over100 = inform100;
+	es.under0 = safeZero;
+	while(!done){
+		int i = 0;
+		std::cin >> i;
+		es.setVal(i);
+		es.process();
+	}
 }
 void eventChecker(){}
